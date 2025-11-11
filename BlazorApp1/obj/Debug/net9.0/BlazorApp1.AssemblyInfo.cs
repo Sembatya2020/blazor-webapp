fe007589d21f511b3467611c7b6d46bad17d186c
Sembatya2020 <ivansmbt@gmail.com>
2025-11-11 13:18:04 +0300

    Refactor code structure for improved readability and maintainability

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cea6df4e43258050ca77c7aef641224d089cbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f310823ba746292a8d1fcce3fd6bff64908ef72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
